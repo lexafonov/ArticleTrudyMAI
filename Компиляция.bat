4f1fb1fa52dece64869f69fcfdabe57118c2df7e
safonov <safonov@is35.ru>
2022-06-19 12:44:50 +0300

    Аннотация, ключевые слова. Нумерация формул и секций.

--- Компиляция.bat
+++ Компиляция.bat
@@ -2,4 +2,7 @@ latex Article.tex
 latex Article.tex
 dvips Article.dvi
 ps2pdf Article.ps
+rem Закрыть открытый документ, чтобы функция корректно собрала новый pdf
+TASKKILL /F /IM STDUViewerApp.exe
+TASKKILL /F /IM msedge.exe
 start Article.pdf
